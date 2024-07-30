4c414b0177bc7e3fdccc4ae70d88c6a9be0cac63
DucMinh0109 <ducminh192005@gmail.com>
2024-07-30 11:12:19 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,22 +7,22 @@ public class Main {
         Dictionary dict = new Dictionary(); // Khởi tạo từ điển mới
         DictionaryManagement dictManager = new DictionaryManagement(dict); // Quản lý từ điển
         DictionaryCommandLine dictCmd = new DictionaryCommandLine(dictManager); // Giao diện dòng lệnh cho từ điển
-
+        dictCmd.dictionaryAdvanced();
         Scanner scanner = new Scanner(System.in); // Scanner để đọc input từ bàn phím
         boolean exit = false; // Biến để kiểm soát việc thoát khỏi vòng lặp
 
         while (!exit) {
-            System.out.println("\nChào mừng bạn đến với Ứng dụng Từ điển!");
-            System.out.println("[0] Thoát");
-            System.out.println("[1] Thêm Từ");
-            System.out.println("[2] Xóa từ");
-            System.out.println("[3] Sửa từ");
-            System.out.println("[4] Hiển thị danh sách các từ");
-            System.out.println("[5] Tra cứu");
-            System.out.println("[6] Tìm kiếm");
-            System.out.println("[7] Nhập danh sách từ vựng từ tệp");
-            System.out.println("[8] Xuất dữ liệu danh sách từ vựng ra tệp");
-            System.out.print("Hãy chọn hành động: ");
+            System.out.println("\nWelcome to My Application!");
+            System.out.println("[0] Exit");
+            System.out.println("[1] Add");
+            System.out.println("[2] Remove");
+            System.out.println("[3] Update");
+            System.out.println("[4] Display");
+            System.out.println("[5] Lookup");
+            System.out.println("[6] Search");
+            System.out.println("[7] Import form file");
+            System.out.println("[8] Export to file");
+            System.out.print("Your action: ");
 
             int choice = scanner.nextInt(); // Đọc lựa chọn của người dùng
             scanner.nextLine();  // Consume newline left-over
@@ -32,13 +32,31 @@ public class Main {
                     exit = true; // Nếu chọn 0, sẽ thoát khỏi vòng lặp
                     break;
                 case 1:
-                    dictCmd.dictionaryBasic(1);
+                    dictCmd.dictionaryAdvanced();
+                    break;
+                case 2:
+                    dictCmd.dictionaryAdvanced();
+                    break;
+                case 3:
+                    dictCmd.dictionaryAdvanced();
                     break;
                 case 4:
-                	dictCmd.dictionaryBasic(4); // Gọi phương thức thêm từ mới và hiển thị tất cả từ
+                    dictCmd.dictionaryAdvanced();
+                    break;
+                case 5:
+                    dictCmd.dictionaryAdvanced();
+                    break;
+                case 6:
+                    dictCmd.dictionaryAdvanced();
+                    break;
+                case 7:
+                    dictCmd.dictionaryAdvanced();
+                    break;
+                case 8:
+                    dictCmd.dictionaryAdvanced();
                     break;
                 default:
-                    System.out.println("Hành động không được hỗ trợ"); // Nếu nhập không đúng
+                    System.out.println("Action not supported"); // Nếu nhập không đúng
                     break;
             }
         }
